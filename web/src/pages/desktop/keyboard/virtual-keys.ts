591d76fa69a821a056370e791a16d67b37633fa4
Takeshi Watanabe <take-cheeze@users.noreply.github.com>
2024-09-19 12:50:51 +0900

    Set capslock and control to correct position in macos virtual keyboard

--- web/src/pages/desktop/keyboard/virtual-keys.ts
+++ web/src/pages/desktop/keyboard/virtual-keys.ts
@@ -15,9 +15,9 @@ export const keyboardOptions = {
       '{escape} F1 F2 F3 F4 F5 F6 F7 F8 F9 F10 F11 F12',
       'Backquote Digit1 Digit2 Digit3 Digit4 Digit5 Digit6 Digit7 Digit8 Digit9 Digit0 Minus Equal {backspace}',
       '{tab} KeyQ KeyW KeyE KeyR KeyT KeyY KeyU KeyI KeyO KeyP BracketLeft BracketRight Backslash',
-      '{capslock} KeyA KeyS KeyD KeyF KeyG KeyH KeyJ KeyK KeyL Semicolon Quote {enter}',
+      '{controlleft} KeyA KeyS KeyD KeyF KeyG KeyH KeyJ KeyK KeyL Semicolon Quote {enter}',
       '{shiftleft} KeyZ KeyX KeyC KeyV KeyB KeyN KeyM Comma Period Slash {shiftright}',
-      '{controlleft} {altleft} {metaleft} {space} {metaright} {altright}'
+      '{capslock} {altleft} {metaleft} {space} {metaright} {altright}'
     ]
   },
   display: {
